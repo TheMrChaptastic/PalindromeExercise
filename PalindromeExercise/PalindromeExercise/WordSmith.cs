a5cfdfb12341b6bd3da4a9a6b5d23c9c48845fc3
TheMrChaptastic <chappy@themrchaptastic.com>
2022-02-01 20:55:52 -0600

    fixed

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string s)
+        {
+            var cArr = s.Replace(" ", "").ToLower().ToCharArray();
+            cArr = cArr.Reverse().ToArray();
+            var nStr = new string(cArr);
+            return nStr == s.Replace(" ", "").ToLower() ? true : false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,15 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("corey", false)]
+        [InlineData("Do geese see God",true)]
+        public void WordSmith(string s, bool expected)
         {
-
+            var testing = new PalindromeExercise.WordSmith();
+            bool actual = testing.IsAPalindrome(s);
+            Assert.Equal(expected, actual);
         }
     }
 }
